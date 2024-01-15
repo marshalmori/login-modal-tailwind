30a04e32b9044207d12c0b6fb990f7cd1a83a8df
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-01-15 17:50:13 -0300

    Login Modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
